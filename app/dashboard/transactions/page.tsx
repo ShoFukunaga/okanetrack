8ea07fbce6813d1f869990587e44fb090bc70f41
ShoFukunaga <squall1tfjr@gmail.com>
2025-03-09 15:31:13 +0900

    add transaction filter

--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -1,10 +1,29 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { getTransactionByMonth } from "@/data/getTransactionByMonth";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { format } from "date-fns";
+import { PencilIcon } from "lucide-react";
 import Link from "next/link";
 import { z } from "zod";
+import { Badge } from "@/components/ui/badge";
+import { getTransactionByMonth } from "@/data/getTransactionByMonth";
+import numeral from "numeral";
+import Filters from "./filters";
+import { getTransactionYearsRange } from "@/data/getTransactionYearRange";
 
 const today = new Date();
 
@@ -27,10 +46,8 @@ export default async function TransactionPage({
   const searchParamsValues = await searchParams;
   const { month, year } = searchSchema.parse(searchParamsValues);
   const selectedDate = new Date(year, month - 1, 1);
-
-  const transactions = getTransactionByMonth({ month, year })
-
-
+  const transactions = await getTransactionByMonth({ month, year })
+  const yearsRange = await getTransactionYearsRange();
   return (
     <div className="max-w-screen-xl mx-auto py-10">
       <Breadcrumb>
@@ -57,16 +74,67 @@ export default async function TransactionPage({
         <CardHeader>
           <CardTitle className="flex justify-between">
             <span>{format(selectedDate, "MMM yyyy")} Transactions</span>
-            <div>
-              dropdowns
-            </div>
+            <Filters year={year} month={month} yearsRange={yearsRange} />
           </CardTitle>
           <CardContent>
             <Button asChild>
               <Link href='/dashboard/transactions/new'>New Transactions</Link>
             </Button>
-            {!!transactions?.length && (
+            {!transactions?.length && (
               <p className="text-center py-10 text-lg text-muted-foreground">There are no transactions for this month</p>
+            )}
+            {!!transactions?.length && (
+              <Table className="mt-4">
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Date</TableHead>
+                    <TableHead>Description</TableHead>
+                    <TableHead>Type</TableHead>
+                    <TableHead>Category</TableHead>
+                    <TableHead>Amount</TableHead>
+                    <TableHead />
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {transactions.map((transaction) => (
+                    <TableRow key={transaction.id}>
+                      <TableCell>
+                        {format(transaction.transactionDate, "do MMM yyyy")}
+                      </TableCell>
+                      <TableCell>{transaction.description}</TableCell>
+                      <TableCell className="capitalize">
+                        <Badge
+                          variant={
+                            transaction.transactionType === "income"
+                              ? "income"
+                              : "expense"
+                          }
+                        >
+                          {transaction.transactionType}
+                        </Badge>
+                      </TableCell>
+                      <TableCell>{transaction.category}</TableCell>
+                      <TableCell>
+                        £{numeral(transaction.amount).format("0,0[.]00")}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        <Button
+                          variant="outline"
+                          asChild
+                          size="icon"
+                          aria-label="Edit transaction"
+                        >
+                          <Link
+                            href={`/dashboard/transactions/${transaction.id}`}
+                          >
+                            <PencilIcon />
+                          </Link>
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
             )}
           </CardContent>
         </CardHeader>
