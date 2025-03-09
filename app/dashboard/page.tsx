cfb7549d99fe34c310c7aec292a9c6aa1c2cda4e
ShoFukunaga <squall1tfjr@gmail.com>
2025-03-09 16:04:07 +0900

    add dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,24 @@
-import Link from "next/link";
+import Cashflow from "./cashflow";
+import RecentTransactions from "./recent-transactions";
 
-export default function DashBoardPage() {
+export default async function DashboardPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ cfyear: string }>;
+}) {
+  const today = new Date();
+  const searchParamsValues = await searchParams;
+  let cfYear = Number(searchParamsValues.cfyear ?? today.getFullYear());
+
+  if (isNaN(cfYear)) {
+    cfYear = today.getFullYear();
+  }
+
   return (
-    <Link href="dashboard/transactions/new">New Transactions</Link>
-  )
+    <div className="max-w-screen-xl mx-auto py-5">
+      <h1 className="text-4xl font-semibold pb-5">Dashboard</h1>
+      <Cashflow year={cfYear} />
+      <RecentTransactions />
+    </div>
+  );
 }
